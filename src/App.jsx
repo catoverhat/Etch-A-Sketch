9ad32aabb606294d8b5e67300b2a714683d78b5e
Daniel <70490862+catoverhat@users.noreply.github.com>
2023-04-26 04:26:30 -0400

    Add clear function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import Grid from "./components/grid";
 
 const App = () => {
-  return <Grid />;
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  const handleMouseDown = () => {
+    setIsMouseDown(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
+  return (
+    <div
+      className="body"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
+      <Grid isMouseDown={isMouseDown} />
+      <footer>Footer</footer>
+    </div>
+  );
 };
 
 export default App;
--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -2,55 +2,69 @@ import { Fragment, useLayoutEffect, useState } from "react";
 import Buttons from "./buttons";
 import "./grid.css";
 
-const Grid = () => {
+const Grid = ({ isMouseDown }) => {
   const [gridSize, setGridSize] = useState(16);
-  const [cells, setCells] = useState("");
-  const [isMouseDown, setIsMouseDown] = useState(false);
+  // const [cells, setCells] = useState("");
+  const [clear, setClear] = useState(false);
+  const [cells, setCells] = useState(Array(gridSize ** 2).fill(false));
+
+  // const gridSizeHandler = (event) => {
+  //   setGridSize(event.target.value);
+  // };
 
   const gridSizeHandler = (event) => {
     setGridSize(event.target.value);
+    setCells(Array(event.target.value ** 2).fill(false));
   };
 
-  useLayoutEffect(() => {
-    const skecht = () => {
-      const gridCells = [];
-      for (let index = 0; index < gridSize ** 2; index++) {
-        gridCells.push(<div key={index}></div>);
-      }
-      // console.log('tst');
-      setCells(gridCells);
-    };
-    skecht();
-  }, [gridSize]);
+  // useLayoutEffect(() => {
+  //   const skecht = () => {
+  //     const gridCells = [];
+  //     for (let index = 0; index < gridSize ** 2; index++) {
+  //       gridCells.push(<div key={index}></div>);
+  //     }
+  //     // console.log('tst');
+  //     setCells(gridCells);
+  //   };
+  //   skecht();
+  // }, [gridSize]);
 
-  const handleMouseDown = () => {
-    setIsMouseDown(true);
-  };
+  // const handleHover = (event) => {
+  //   if (isMouseDown) {
+  //     event.target.classList.add("active");
+  //   }
+  // };
 
-  const handleMouseUp = () => {
-    setIsMouseDown(false);
-  };
-
-  const handleHover = (event) => {
+  const handleHover = (index) => {
     if (isMouseDown) {
-      event.target.classList.add("active");
+      setCells((prevState) => {
+        const newState = [...prevState];
+        newState[index] = true;
+        return newState;
+      });
     }
   };
 
   const clearGrid = () => {
-    setCells([]);
+    setCells(Array(gridSize ** 2).fill(false));
   };
 
   return (
     <Fragment>
       <div
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseOver={handleHover}
+        // onMouseOver={handleHover}
         className="grid-container"
         style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
       >
-        {cells}
+        {/* {cells} */}
+
+        {cells.map((active, index) => (
+          <div
+            key={index}
+            onMouseOver={() => handleHover(index)}
+            className={active ? "active" : ""}
+          ></div>
+        ))}
       </div>
 
       <Buttons
